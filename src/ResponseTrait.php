c6a1132b93e27a2d747d0310c418683ba5d2ec39
Ilya Rukavishnikov <ilya-ndt@mail.ru>
2025-11-14 18:08:15 +0300

    [fix]: Status code and message from RFC 9110

--- src/ResponseTrait.php
+++ src/ResponseTrait.php
@@ -14,9 +14,11 @@ trait ResponseTrait
      * @var string[]
      */
     private static array $reasonPhraseList = [
+        // 1xx (Informational)
         100 => 'Continue',
         101 => 'Switching Protocols',
 
+        // 2xx (Successful)
         200 => 'OK',
         201 => 'Created',
         202 => 'Accepted',
@@ -25,6 +27,7 @@ trait ResponseTrait
         205 => 'Reset Content',
         206 => 'Partial Content',
 
+        // 3xx (Redirection)
         300 => 'Multiple Choices',
         301 => 'Moved Permanently',
         302 => 'Found',
@@ -32,7 +35,9 @@ trait ResponseTrait
         304 => 'Not Modified',
         305 => 'Use Proxy',
         307 => 'Temporary Redirect',
+        308 => 'Permanent Redirect',
 
+        // 4xx (Client Error)
         400 => 'Bad Request',
         401 => 'Unauthorized',
         402 => 'Payment Required',
@@ -41,23 +46,27 @@ trait ResponseTrait
         405 => 'Method Not Allowed',
         406 => 'Not Acceptable',
         407 => 'Proxy Authentication Required',
-        408 => 'Request Time-out',
+        408 => 'Request Timeout',
         409 => 'Conflict',
         410 => 'Gone',
         411 => 'Length Required',
         412 => 'Precondition Failed',
-        413 => 'Request Entity Too Large',
-        414 => 'Request-URI Too Large',
+        413 => 'Content Too Large',
+        414 => 'URI Too Long',
         415 => 'Unsupported Media Type',
-        416 => 'Requested range not satisfiable',
+        416 => 'Range Not Satisfiable',
         417 => 'Expectation Failed',
+        421 => 'Misdirected Request',
+        422 => 'Unprocessable Content',
+        426 => 'Upgrade Required',
 
+        // 5xx (Server Error)
         500 => 'Internal Server Error',
         501 => 'Not Implemented',
         502 => 'Bad Gateway',
         503 => 'Service Unavailable',
-        504 => 'Gateway Time-out',
-        505 => 'HTTP Version not supported',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
     ];
 
     /**
